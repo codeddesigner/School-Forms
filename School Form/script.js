bb9190aadaf05a89ae3275ec9227ea313bff773e
Chidera <chideranwanyemike84@gmail.com>
2020-03-26 14:31:07 +0100

    School Forms

---
+++ School Form/script.js
@@ -0,0 +1,8 @@
+let register = document.querySelectorAll("input").addEventListener("click", function(e){
+    e.target.element.value
+    preventDefault()
+    if( register.length > 0){
+       alert("love ya")
+    }
+
+})
